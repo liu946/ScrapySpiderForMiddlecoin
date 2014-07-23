fef0c8bf3360d7abef087abb82526ac1475446d6
liu946 <liuyang570926881@gmail.com>
2014-07-23 12:42:34 +0800

    避免编译文件提交

--- middlecoin/middlecoin/items.py
+++ middlecoin/middlecoin/items.py
@@ -11,4 +11,10 @@ import scrapy
 class MiddlecoinItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+	Username = scrapy.Field()    
+	Accepted = scrapy.Field()
+	Rejected = scrapy.Field()
+	ImmatureUnexchangedBalance = scrapy.Field()
+	UnexchangedBalance = scrapy.Field()
+	Balance = scrapy.Field()
+	PaidOut = scrapy.Field()
